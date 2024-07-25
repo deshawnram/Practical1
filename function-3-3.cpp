efb3d7052477334f48723a6ee3e591345919d3ff
deshawnram <a1925401@adelaide.edu.au>
2024-07-25 11:44:31 +0930

    commit again

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -3,13 +3,18 @@
 
 double weighted_average(int array[], int n) {
     if (n < 1) return 0.0;
+    
     std::unordered_map<int, int> frequency;
+    double weighted_sum = 0.0;
+
+
     for (int i = 0; i < n; ++i) {
         frequency[array[i]]++;
     }
-    double weighted_sum = 0.0;
-    for (auto& pair : frequency) {
+
+    for (const auto& pair : frequency) {
         weighted_sum += pair.first * pair.second;
     }
+
     return weighted_sum / n;
 }
