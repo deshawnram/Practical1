ec69adc06f69af884be0193bfd17a99bf72452bd
deshawnram <a1925401@adelaide.edu.au>
2024-07-25 11:46:08 +0930

    commit function 3

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -2,19 +2,12 @@
 #include <unordered_map>
 
 double weighted_average(int array[], int n) {
-    if (n < 1) return 0.0;
+    if (n == 0) return 0.0; 
     
-    std::unordered_map<int, int> frequency;
-    double weighted_sum = 0.0;
-
+    double sum = 0.0;
 
     for (int i = 0; i < n; ++i) {
-        frequency[array[i]]++;
+        sum += array[i];
     }
-
-    for (const auto& pair : frequency) {
-        weighted_sum += pair.first * pair.second;
-    }
-
-    return weighted_sum / n;
+    return sum / n;
 }
