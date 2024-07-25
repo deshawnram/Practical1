027f233998dcf7d9e3b47f3b018d2b712d39e4a6
deshawnram <a1925401@adelaide.edu.au>
2024-07-25 11:42:02 +0930

    commit again

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -3,7 +3,12 @@
 bool is_fanarray(int array[], int n) {
     if (n < 1) return false;
     for (int i = 0; i < n / 2; ++i) {
-        if (array[i] != array[n - i - 1] || (i > 0 && array[i] < array[i - 1])) {
+        if (array[i] != array[n - i - 1]) {
+            return false;
+        }
+    }
+    for (int i = 1; i <= n / 2; ++i) {
+        if (array[i] < array[i - 1]) {
             return false;
         }
     }
